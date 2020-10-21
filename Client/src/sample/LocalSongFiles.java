c7e8373f46b565742402840373ca4d849b27cd7d
Snigdha Sharma <snigdhas2012@gmail.com>
2020-10-21 15:13:33 +0530

    Local Songs Fetched in text file

--- Client/src/sample/LocalSongFiles.java
+++ Client/src/sample/LocalSongFiles.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Stack;
 
 public class LocalSongFiles
 {
@@ -23,5 +24,69 @@ public class LocalSongFiles
         {
             e.printStackTrace();
         }
+        file=new File("./LocalSongs.txt");
+        try
+        {
+            writer=new FileWriter(file);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void searchLocalSongs()
+    {
+        Stack<File> path=new Stack<File>();
+        File[] init=File.listRoots();
+        for (File x:init)
+        {
+            path.push(x);
+            System.out.println(x.getAbsolutePath());
+        }
+        while(!path.empty())
+        {
+            File temp=path.peek();
+            path.pop();
+            System.out.println(temp);
+            File[] subFiles=temp.listFiles();
+            //System.out.println(subFiles);
+            if (subFiles!=null)
+            {
+                for (File x:subFiles)
+                {
+                    if (x.getName().endsWith(".mp3"))
+                    {
+                        try
+                        {
+                            writer.write(x.getAbsolutePath());
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if (x.isDirectory())
+                    {
+                        path.push(x);
+                    }
+                }
+            }
+        }
+        try
+        {
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[]args)
+    {
+        LocalSongFiles l=new LocalSongFiles();
+        l.makeFreshFile();
+        l.searchLocalSongs();
     }
 }
